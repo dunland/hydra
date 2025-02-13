52a2f032efda98d5612d94d5800a052fa6db4cc6
David Unland <david.unland@web.de>
2025-02-13 09:02:20 +0100

    disable remote language load

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const devtools = require('choo-devtools')
 const choo = require('choo')
 const store = require('./src/stores/store.js')
-const languageStore = require('./src/stores/language-store.js') // TODO: deactivate, since it uses network connection
+// const serialServer = require('./utils/server.js')
+const languageStore = require('./src/stores/language-store.js')
 
 const mainView = require('./src/views/main.js')
 
-console.log("hello entry point");
 // navigator.serial.requestPorts()
 
 const app = choo()
 app.use(devtools())
 app.use(store)
-app.use(languageStore) // TODO: deactivate, since it uses network connection. It has to be changed in info.js, too
+app.use(languageStore) // TODO: deactivate, since it depends on network connection. It has to be changed in info.js, too... done?
 app.route('/', mainView)
 app.route('/hydra-backup', mainView)
 app.mount('body')
